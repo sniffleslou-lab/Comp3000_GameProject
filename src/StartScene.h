44eff5eae7c2bada728f0730622d0f4b7ade1f13
GwyfynSimple <sniffleslou@gmail.com>
2025-11-18 11:10:41 +0000

    created shells + updated gitnore
    weird mac stuff with ds_store
    
    going to finish scene control, so i can start creating multiple scenes throughout the game

---
+++ src/StartScene.h
@@ -0,0 +1,8 @@
+//
+// Created by Lou halpin on 18/11/2025.
+//
+
+#ifndef AMIPRETTY_STARTSCENE_H
+#define AMIPRETTY_STARTSCENE_H
+
+#endif //AMIPRETTY_STARTSCENE_H
---
+++ src/startScene.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Lou halpin on 18/11/2025.
+//
